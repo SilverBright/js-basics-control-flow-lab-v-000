7b8c77d88110f6d0569eac67d2d9db72e2989b7b
Barbara Washington <barbara.m.washington@gmail.com>
2019-01-07 23:46:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Write your code in this file!
 
-function scuberGreetingForFeet(199) {
+function scuberGreetingForFeet() {
   //this is where we can use conditionals given our argument, someValue
   //don't forget to return whatever the result is!
 }
