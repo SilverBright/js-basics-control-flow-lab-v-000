ecf6ef7cadb417fb6fd2c6486e315f55eb1c4533
Barbara Washington <barbara.m.washington@gmail.com>
2019-01-08 00:07:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Write your code in this file!
 
-function scuberGreetingForFeet(feet) {
+function scuberGreetingForFeet(distance) {
   let freeRide
   if (feet <= 400) {
     freeRide = 'This one is on me!';
